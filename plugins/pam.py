e09c0c7ed8e0a0bd9b346ce2357c7f69a985b108
Robert Andersson <rbrt.andersson@outlook.com>
2018-11-28 20:15:06 +0100

    improve pam plugin, slightly...
    Signed-off-by: Robert Andersson <rbrt.andersson@outlook.com>

--- plugins/pam.py
+++ plugins/pam.py
@@ -8,13 +8,28 @@ import toolchains.pam
 
 class CXXExecutable(Task, HashInfluenceProvider):
     sources = []
+    macros = []
+    incpaths = []
+    libpaths = []
+    features = []
+    libraries = []
 
     def __init__(self, *args, **kwargs):
         super(CXXExecutable, self).__init__(*args, **kwargs)
         self.influence.append(self)
+        self.sources = utils.as_list(utils.call_or_return(self, self.__class__.sources))
+        sources = []
+        for l in map(self.tools.glob, self.sources):
+            sources += l
+        self.sources = sources
         self.project = build.model.cxx_executable(
             self.name,
-            sources=self.sources
+            sources=self.sources,
+            macros=self.macros,
+            incpaths=self.incpaths,
+            libpaths=self.libpaths,
+            features=self.features,
+            libraries=self.libraries
         )
         self.toolchain = toolchains.pam.pam_gnu_toolchain("pam-gcc")
         self.cxx_project = self.toolchain.generate(self.project)
