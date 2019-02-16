a141e815b35937e88d4e56b394ab5b27cd056e6e
Robert Andersson <rbrt.andersson@outlook.com>
2019-02-16 16:26:16 +0100

    colors: not disabled if custom config file is loaded
    Signed-off-by: Robert Andersson <rbrt.andersson@outlook.com>

--- jolt/colors.py
+++ jolt/colors.py
@@ -3,19 +3,20 @@ from colorama import Fore, Style
 from jolt import config
 
 
-enabled = config.getboolean("jolt", "colors", True)
+def enabled():
+    return config.getboolean("jolt", "colors", True)
 
 def red(s):
-    return Fore.RED + Style.BRIGHT + s + Style.RESET_ALL if enabled else s
+    return Fore.RED + Style.BRIGHT + s + Style.RESET_ALL if enabled() else s
 
 def yellow(s):
-    return Fore.YELLOW + Style.BRIGHT + s + Style.RESET_ALL if enabled else s
+    return Fore.YELLOW + Style.BRIGHT + s + Style.RESET_ALL if enabled() else s
 
 def green(s):
-    return Fore.GREEN + Style.BRIGHT + s + Style.RESET_ALL if enabled else s
+    return Fore.GREEN + Style.BRIGHT + s + Style.RESET_ALL if enabled() else s
 
 def blue(s):
-    return Fore.BLUE + Style.BRIGHT + s + Style.RESET_ALL if enabled else s
+    return Fore.BLUE + Style.BRIGHT + s + Style.RESET_ALL if enabled() else s
 
 def bright(s):
-    return Style.BRIGHT + s + Style.RESET_ALL if enabled else s
+    return Style.BRIGHT + s + Style.RESET_ALL if enabled() else s
