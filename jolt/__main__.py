8b1c4abd3dd792053ca7096e8b5da04004f8df0f
Robert Andersson <rbrt.andersson@outlook.com>
2019-04-10 12:18:38 +0200

    log: workaround callstack format string issue
    Signed-off-by: Robert Andersson <rbrt.andersson@outlook.com>

--- jolt/__main__.py
+++ jolt/__main__.py
@@ -15,7 +15,7 @@ def main():
     signal.signal(signal.SIGUSR1, start_pdb)
 
     try:
-        cli.cli(obj={})
+        cli.cli(obj=dict())
     except KeyboardInterrupt as e:
         log.warn("Interrupted by user")
         sys.exit(1)
