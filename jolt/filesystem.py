819cbec11597e500fb01511d25858dac2c31a319
Robert Andersson <robert.m.andersson@se.atlascopco.com>
2019-06-19 14:41:14 +0200

    filesystem: copy() should handle existing destination path
    Change-Id: I0f66150911a222a3368210ae6bd2c60ff9a2830d
    Signed-off-by: Robert Andersson <robert.m.andersson@se.atlascopco.com>

--- jolt/filesystem.py
+++ jolt/filesystem.py
@@ -41,6 +41,45 @@ def symlink(src, dest, *args, **kwargs):
     else:
         os.symlink(src, dest, *args, **kwargs)
 
+def copytree(src, dst, symlinks=False, ignore=None):
+    names = os.listdir(src)
+    if ignore is not None:
+        ignored_names = ignore(src, names)
+    else:
+        ignored_names = set()
+
+    makedirs(dst)
+    errors = []
+    for name in names:
+        if name in ignored_names:
+            continue
+        srcname = os.path.join(src, name)
+        dstname = os.path.join(dst, name)
+        try:
+            if symlinks and os.path.islink(srcname):
+                linkto = os.readlink(srcname)
+                os.symlink(linkto, dstname)
+            elif os.path.isdir(srcname):
+                copytree(srcname, dstname, symlinks, ignore)
+            else:
+                shutil.copy2(srcname, dstname)
+            # XXX What about devices, sockets etc.?
+        except (IOError, os.error) as why:
+            errors.append((srcname, dstname, str(why)))
+        # catch the Error from the recursive copytree so that we can
+        # continue with other files
+        except Error as err:
+            errors.extend(err.args[0])
+    try:
+        shutil.copystat(src, dst)
+    except WindowsError:
+        # can't copy file access times on Windows
+        pass
+    except OSError as why:
+        errors.extend((src, dst, str(why)))
+    if errors:
+        raise Error(errors)
+
 def copy(src, dest, symlinks=False):
     if not path.exists(dest):
         if dest[-1] == os.sep:
@@ -53,7 +92,7 @@ def copy(src, dest, symlinks=False):
             dest = path.join(dest, path.basename(src))
 
     if path.isdir(src):
-        shutil.copytree(src, dest, symlinks=symlinks)
+        copytree(src, dest, symlinks=symlinks)
     else:
         shutil.copy2(src, dest)
 
