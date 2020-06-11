5657a3792e2382de747ace8f23b9df6404445f46
Robert Andersson <robert.m.andersson@atlascopco.com>
2020-06-11 14:52:20 +0200

    ninja-cache: refactor to use the task hook api
    Change-Id: I0dea03bb2f52a8004bb935b09065a86001291071
    Signed-off-by: Robert Andersson <robert.m.andersson@atlascopco.com>

--- jolt/plugins/ninja-cache.py
+++ jolt/plugins/ninja-cache.py
@@ -12,39 +12,53 @@ from jolt import filesystem as fs
 from jolt import log
 from jolt import tools
 from jolt import utils
+from jolt.hooks import TaskHook, TaskHookFactory
+from jolt.influence import StringInfluence
 from jolt.plugins import ninja
 from jolt.plugins import ninjacli
 
 log.verbose("[NinjaCache] Loaded")
 
 
-def run_cache(self, artifact, tools):
-    cli = fs.path.join(fs.path.dirname(__file__), "ninjacli.py")
-    disabled = config.getboolean("ninja-cache", "disable", False)
+class CacheHooks(TaskHook):
+    def task_created(self, task):
+        if not isinstance(task.task, ninja.CXXLibrary) or task.task.shared:
+            return
+        task.task.influence.append(StringInfluence("NinjaCache"))
 
-    tools.setenv("CCWRAP", "{} {} -- ".format(sys.executable, cli))
-    tools.setenv("CXXWRAP", "{} {} -- ".format(sys.executable, cli))
-    tools.setenv("JOLT_CACHEDIR", cache.ArtifactCache.get().root)
-    tools.setenv("JOLT_CANONTASK", utils.canonical(self.name))
-    tools.setenv("NINJACACHE_DISABLE", "1" if disabled else "0")
-    tools.setenv("NINJACACHE_MAXARTIFACTS", config.getint("ninja-cache", "maxartifacts", 0))
-    if log.is_verbose():
-        tools.setenv("NINJACACHE_VERBOSE", "1")
+    def task_prerun(self, task, deps, tools):
+        if not isinstance(task.task, ninja.CXXLibrary) or task.task.shared:
+            return
 
-    if not disabled:
-        objcache = ninjacli.Cache(tools.builddir("ninja", self.incremental))
-        objcache.load_manifests(tools.getenv("JOLT_CACHEDIR"), tools.getenv("JOLT_CANONTASK"))
-        objcache.save()
+        cli = fs.path.join(fs.path.dirname(__file__), "ninjacli.py")
+        disabled = config.getboolean("ninja-cache", "disable", False)
+
+        tools.setenv("CCWRAP", "{} {} -- ".format(sys.executable, cli))
+        tools.setenv("CXXWRAP", "{} {} -- ".format(sys.executable, cli))
+        tools.setenv("JOLT_CACHEDIR", cache.ArtifactCache.get().root)
+        tools.setenv("JOLT_CANONTASK", utils.canonical(task.task.name))
+        tools.setenv("NINJACACHE_DISABLE", "1" if disabled else "0")
+        tools.setenv("NINJACACHE_MAXARTIFACTS", config.getint("ninja-cache", "maxartifacts", 0))
+        if log.is_verbose():
+            tools.setenv("NINJACACHE_VERBOSE", "1")
+
+        if not disabled:
+            objcache = ninjacli.Cache(tools.builddir("ninja", task.task.incremental))
+            objcache.load_manifests(tools.getenv("JOLT_CACHEDIR"), tools.getenv("JOLT_CANONTASK"))
+            objcache.save()
+
+    def task_postpublish(self, task, artifact, tools):
+        if not isinstance(task.task, ninja.CXXLibrary) or task.task.shared:
+            return
+        with tools.cwd(task.task.outdir):
+            m = ninjacli.LibraryManifest(tools.expand_path(".ninja.json"))
+            m.read()
+            m.add_library(fs.path.join(task.task.publishdir, fs.path.basename(task.task.outfiles[0])))
+            m.write()
+            artifact.collect(".ninja.json")
 
 
-def publish_cache(self, artifact, tools):
-    with tools.cwd(self.outdir):
-        m = ninjacli.LibraryManifest(tools.expand_path(".ninja.json"))
-        m.read()
-        m.add_library(fs.path.join(self.publishdir, fs.path.basename(self.outfiles[0])))
-        m.write()
-        artifact.collect(".ninja.json")
-
-ninja.CXXLibrary.run = utils.decorate_prepend(ninja.CXXLibrary.run, run_cache)
-ninja.CXXLibrary.publish = utils.decorate_prepend(ninja.CXXLibrary.publish, publish_cache)
-ninja.CXXLibrary.shell = utils.decorate_prepend(ninja.CXXLibrary.shell, run_cache)
+@TaskHookFactory.register
+class CacheHookFactory(TaskHookFactory):
+    def create(self, env):
+        return CacheHooks()
