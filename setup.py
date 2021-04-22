40cd9bdb72285b94bede698d957b9bffe6a30648
Robert Andersson <robert.m.andersson@atlascopco.com>
2021-04-22 17:34:44 +0200

    setup: include xslt data files
    Signed-off-by: Robert Andersson <robert.m.andersson@atlascopco.com>
    Change-Id: I9ba7e6da186148b29b1ae4caa519a915651a5635

--- setup.py
+++ setup.py
@@ -10,91 +10,35 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='jolt',
-
-    # Versions should comply with PEP440. For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
     version='0.9.12',
-
     description='A task executor',
     long_description=long_description,
-
-    # The project's main homepage.
     url='https://bitbucket.org/rand_r/jolt',
-
-    # Author details
     author='Robert Andersson',
     author_email='rand_r@bitbucket.org',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 4 - Beta',
-
-        # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
     ],
-
-    # What does your project relate to?
     keywords='build msbuild xcode make pam bazel jolt conan',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
     install_requires=["requests", "click>=7.0", "networkx", "keyring", "keyrings.alt",
                       "jinja2", "python-jenkins", "multi_key_dict",
                       "tqdm<=4.29.1", "bz2file", "colorama", "ninja-syntax",
                       "pyyaml", "fasteners", "ntfsutils", "pygit2", "lxml"],
-
     dependency_links=[],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
     extras_require={
         'allure': ['allure-python-commons'],
         'amqp': ['pika'],
         'dev': ['check-manifest'],
         'test': ['coverage'],
     },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
     package_data={
-    #    'sample': ['package_data.dat'],
+        'jolt': ['**/*.xslt'],
     },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[('my_data', ['data/data_file'])],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
             'jolt=jolt.__main__:main',
