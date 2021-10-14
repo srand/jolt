37a07f4b203493d168d2958df631f5d5df86aaa0
Robert Andersson <robert.m.andersson@atlascopco.com>
2021-10-14 13:51:57 +0200

    googletest: report test errors
    Signed-off-by: Robert Andersson <robert.m.andersson@atlascopco.com>
    Change-Id: I5faf9be49d64cd1e38f11f622ce1940b4b8ed1b3

--- jolt/plugins/googletest.py
+++ jolt/plugins/googletest.py
@@ -1,5 +1,10 @@
 from xml.etree import ElementTree as ET
 
+
+def _tcname(suite, case):
+    return "{}.{}".format(suite.get("name"), case.get("name"))
+
+
 def import_failures(xml, report):
     tree = ET.parse(xml)
     root = tree.getroot()
@@ -7,10 +12,14 @@ def import_failures(xml, report):
     for testsuite in testsuites:
         testcases = testsuite.findall(".//testcase")
         for testcase in testcases:
+            errors = testcase.findall(".//error")
+            for error in errors:
+                name = _tcname(testsuite, testcase)
+                message = error.text
+                report.add_error("Test Error", name, message)
+
             failures = testcase.findall(".//failure")
             for failure in failures:
-                tsname = testsuite.get("name")
-                tcname = testcase.get("name")
-                name = tsname + "." + tcname
+                name = _tcname(testsuite, testcase)
                 message = failure.text
                 report.add_error("Test Failed", name, message)
