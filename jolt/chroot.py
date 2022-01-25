28a445a3aa0897d089e7ce272cec3e6f34b2abf3
Robert Andersson <robert.m.andersson@atlascopco.com>
2022-01-25 17:49:49 +0100

    tools: mount /dev, /proc and /home when chroot():ed
    Signed-off-by: Robert Andersson <robert.m.andersson@atlascopco.com>
    Change-Id: I931476257db4a7228ede7de9ac827b1d1c7185fb

--- jolt/chroot.py
+++ jolt/chroot.py
@@ -8,10 +8,17 @@ chroot = sys.argv[1]
 joltdir = os.environ["JOLTDIR"]
 cachedir = os.environ["JOLTCACHEDIR"]
 cwd = os.environ["PWD"]
+home = os.environ["HOME"]
 
+subprocess.run(["mount", "--rbind", "/dev", chroot + "/dev"])
+subprocess.run(["mount", "--rbind", "/proc", chroot + "/proc"])
+
+subprocess.run(["mkdir", "-p", chroot + home])
 subprocess.run(["mkdir", "-p", chroot + joltdir])
 subprocess.run(["mkdir", "-p", chroot + cachedir])
-subprocess.run(["mount", "--rbind", joltdir, chroot + joltdir])
+subprocess.run(["mount", "--rbind", home, chroot + home])
 subprocess.run(["mount", "--rbind", cachedir, chroot + cachedir])
+subprocess.run(["mount", "--rbind", joltdir, chroot + joltdir])
+
 os.chroot(chroot)
 sys.exit(subprocess.run(sys.argv[2:], cwd=cwd, shell=True).returncode)
