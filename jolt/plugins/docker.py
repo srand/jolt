974c78ecf5031dbbd1dc2f156e3a3867c2d011ab
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-07 06:53:03 +0000

    Adding tarfile member sanitization to extractall()

--- jolt/plugins/docker.py
+++ jolt/plugins/docker.py
@@ -516,7 +516,29 @@ class DockerImage(Task):
         targetpath = tools.expand_path(targetpath)
 
         with _Tarfile.open(layerpath, 'r') as tar:
-            tar.extractall(targetpath)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner) 
+                
+            
+            safe_extract(tar, targetpath)
 
     @property
     def _buildargs(self):
