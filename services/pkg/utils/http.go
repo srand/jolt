b2c45bdf077352c646cee128586c06ee5d1deba2
Robert Andersson <robert.m.andersson@atlascopco.com>
2024-02-05 13:42:07 +0100

    service/cache: log errors
    Change-Id: Ia66f7e0ee0981f40c55dd34d60332e36ccf30a3e
    Signed-off-by: Robert Andersson <robert.m.andersson@atlascopco.com>

--- services/pkg/utils/http.go
+++ services/pkg/utils/http.go
@@ -8,6 +8,9 @@ import (
 func HttpLogger(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		err := next(c)
+		if err != nil {
+			log.Errorf("%4s %s %v", c.Request().Method, c.Request().URL, err)
+		}
 		log.Tracef("%4s %s %v", c.Request().Method, c.Request().URL, c.Response().Status)
 		return err
 	}
