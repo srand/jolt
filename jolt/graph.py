0cd2be16d5080da89912e60594177ae6e3a2dcc4
Robert Andersson <robert.m.andersson@atlascopco.com>
2024-10-17 11:06:03 +0200

    graph: don't discard resource artifacts
    Signed-off-by: Robert Andersson <robert.m.andersson@atlascopco.com>

--- jolt/graph.py
+++ jolt/graph.py
@@ -624,7 +624,7 @@ class TaskProxy(object):
                     if self.task.is_runnable():
                         log.verbose("Host: {0}", getenv("HOSTNAME", "localhost"))
 
-                    with self.lock_artifacts(discard=True) as artifacts:
+                    with self.lock_artifacts(discard=not self.is_resource()) as artifacts:
                         exitstack = ExitStack()
 
                         # Indicates whether session artifacts have been published
