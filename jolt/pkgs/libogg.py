29114b16b650480411e7bc0047fd1f79368be2e4
Robert Andersson <rbrt.andersson@outlook.com>
2025-12-22 22:12:05 +0100

    pkgs: add libogg and libvorbis
    Signed-off-by: Robert Andersson <rbrt.andersson@outlook.com>

---
+++ jolt/pkgs/libogg.py
@@ -0,0 +1,18 @@
+from jolt import attributes, Parameter
+from jolt.pkgs import cmake
+from jolt.plugins import git, cmake
+from jolt.tasks import TaskRegistry
+
+
+@attributes.requires("requires_cmake")
+@attributes.requires("requires_git")
+class Libogg(cmake.CMake):
+    name = "libogg"
+    version = Parameter("1.3.6", help="libogg version.")
+    options = ["CMAKE_POLICY_VERSION_MINIMUM=3.5"]
+    requires_cmake = ["cmake"]
+    requires_git = ["git:url=https://github.com/xiph/ogg.git,rev=v{version}"]
+    srcdir = "{git[ogg]}"
+
+
+TaskRegistry.get().add_task_class(Libogg)
---
+++ jolt/pkgs/libvorbis.py
@@ -0,0 +1,20 @@
+from jolt import attributes, Parameter
+from jolt.pkgs import cmake, libogg
+from jolt.plugins import git, cmake
+from jolt.tasks import TaskRegistry
+
+
+@attributes.requires("requires_cmake")
+@attributes.requires("requires_git")
+@attributes.requires("requires_ogg")
+class Libvorbis(cmake.CMake):
+    name = "libvorbis"
+    version = Parameter("1.3.7", help="libvorbis version.")
+    options = ["CMAKE_POLICY_VERSION_MINIMUM=3.5"]
+    requires_cmake = ["cmake"]
+    requires_git = ["git:url=https://github.com/xiph/vorbis.git,rev=v{version}"]
+    requires_ogg = ["libogg"]
+    srcdir = "{git[vorbis]}"
+
+
+TaskRegistry.get().add_task_class(Libvorbis)
