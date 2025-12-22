4b97eb261052ef584a5a78660b0fd8508f935379
Robert Andersson <rbrt.andersson@outlook.com>
2025-12-22 22:32:20 +0100

    pkgs: add qt protobuf dep
    Signed-off-by: Robert Andersson <rbrt.andersson@outlook.com>

--- jolt/pkgs/qt.py
+++ jolt/pkgs/qt.py
@@ -1,5 +1,5 @@
 from jolt import attributes, Parameter, BooleanParameter
-from jolt.pkgs import cmake, dbus, ninja, libglvnd
+from jolt.pkgs import cmake, dbus, ninja, libglvnd, protobuf
 from jolt.plugins import git, cmake
 from jolt.tasks import TaskRegistry
 
@@ -7,10 +7,9 @@ from jolt.tasks import TaskRegistry
 @attributes.requires("requires_cmake")
 @attributes.requires("requires_dbus")
 @attributes.requires("requires_git")
-@attributes.requires("requires_ninja")
-@attributes.requires("requires_{system}")
 @attributes.requires("requires_gl_{gl}")
-@attributes.system
+@attributes.requires("requires_ninja")
+@attributes.requires("requires_protobuf")
 @attributes.attribute("options", "options_gl_{gl}")
 class Qt(cmake.CMake):
     name = "qt"
@@ -23,6 +22,8 @@ class Qt(cmake.CMake):
     requires_git = ["git:url=https://github.com/qt/qt5.git,path=build/git-qt,rev=v{version},submodules=true"]
     requires_ninja = ["ninja"]
     requires_gl_true = ["libglvnd"]
+    requires_protobuf = ["protobuf"]
+    srcdir = "{git[qt5]}"
 
     options_gl_true = [
         "CMAKE_PREFIX_PATH={deps[libglvnd].path}",
@@ -34,11 +35,6 @@ class Qt(cmake.CMake):
         "OPENGL_glx_LIBRARY={deps[libglvnd].path}/lib/libGLX.so",
         "OPENGL_glx_INCLUDE_DIR={deps[libglvnd].path}/include",
     ]
-
-    def run(self, deps, tools):
-        self.deps = deps
-        with tools.cwd("{git[qt5]}"):
-            super().run(deps, tools)
 
 
 TaskRegistry.get().add_task_class(Qt)
