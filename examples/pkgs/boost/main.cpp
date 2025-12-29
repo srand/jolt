5501d6d21b01df14697c96041bd0350ceadcdc2e
Robert Andersson <rbrt.andersson@outlook.com>
2025-12-29 17:51:34 +0100

    examples: pkgs: add boost consumption example
    Signed-off-by: Robert Andersson <rbrt.andersson@outlook.com>

---
+++ examples/pkgs/boost/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <boost/version.hpp>
+
+
+int main() {
+    std::cout << "Boost version: " << BOOST_LIB_VERSION << std::endl;
+    return 0;
+}
--- jolt/pkgs/boost.py
+++ jolt/pkgs/boost.py
@@ -1,3 +1,4 @@
+import os
 from jolt import attributes, Parameter
 from jolt.plugins import git
 from jolt.tasks import Task, TaskRegistry
@@ -20,6 +21,16 @@ class Boost(Task):
     def publish(self, artifact, tools):
         with tools.cwd(self.installdir):
             artifact.collect("*", symlinks=True)
+        artifact.cxxinfo.incpaths.append("include")
+        artifact.cxxinfo.libpaths.append("lib")
+
+        with tools.cwd(self.installdir + "/lib"):
+            for lib in tools.glob("libboost_*.a"):
+                name, _ = os.path.splitext(os.path.basename(lib))
+                artifact.cxxinfo.libraries.append(name[3:])
+            for lib in tools.glob("boost_*.lib"):
+                name, _ = os.path.splitext(os.path.basename(lib))
+                artifact.cxxinfo.libraries.append(name)
 
 
 TaskRegistry.get().add_task_class(Boost)
