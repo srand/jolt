100dff4d088358cd49824c7d60616448093eabeb
Robert Andersson <robert.m.andersson@atlascopco.com>
2026-01-05 22:55:03 +0100

    cmake: add options() and requires() decorators
    Signed-off-by: Robert Andersson <robert.m.andersson@atlascopco.com>

--- jolt/plugins/cmake.py
+++ jolt/plugins/cmake.py
@@ -7,7 +7,53 @@ from jolt.error import raise_task_error_if
 import os
 
 
+def options(attrib):
+    """
+    Decorates a CMake task with an alternative ``options`` attribute.
+
+    The new attribute will be concatenated with the regular
+    ``options`` attribute.
+
+    Args:
+        attrib (str): Name of alternative attribute.
+            Keywords are expanded.
+    """
+    return utils.concat_attributes("options", attrib)
+
+
+def requires(version=None):
+    """ Decorator to add CMake requirements to a task. """
+
+    import jolt.pkgs.cmake
+
+    def decorate(cls):
+        cls = attributes.requires("requires_cmake")(cls)
+        cls.requires_cmake = ["cmake" + (f":version={version}" if version else "")]
+        return cls
+
+    return decorate
+
+
+def use_ninja():
+    """
+    Decorator to add Ninja dependencies to CMake task.
+
+    It also selects Ninja as the CMake generator and builder.
+    """
+
+    import jolt.pkgs.ninja
+
+    def decorate(cls):
+        cls = attributes.requires("requires_ninja")(cls)
+        cls.generator = "Ninja"
+        cls.requires_ninja = ["ninja"]
+        return cls
+
+    return decorate
+
+
 @attributes.common_metadata()
+@options("options")
 class CMake(Task):
     """ Builds and publishes a CMake project """
     abstract = True
@@ -38,14 +84,15 @@ class CMake(Task):
         self.deps = deps
         raise_task_error_if(not self.cmakelists, self, "cmakelists attribute has not been defined")
 
-        options = self.options
-        options += ["CMAKE_BUILD_TYPE=" + str(self.config)]
+        config = tools.expand(str(self.config))
+        options = self._options()
+        options += ["CMAKE_BUILD_TYPE=" + config]
 
         with tools.cwd(self.srcdir or self.joltdir):
             cmake = tools.cmake(deps, incremental=self.incremental)
             cmake.configure(tools.expand(self.cmakelists), *["-D" + tools.expand(option) for option in options], generator=self.generator)
-            cmake.build()
-            cmake.install()
+            cmake.build(config=config)
+            cmake.install(config=config)
 
     def publish(self, artifact, tools):
         cmake = tools.cmake(incremental=self.incremental)
@@ -109,16 +156,3 @@ class CXXExecutable(_CMakeCXX):
 
     def publish(self, artifact, tools):
         super().publish(artifact, tools)
-
-
-def requires(version=None):
-    """ Decorator to add CMake requirements to a task. """
-
-    import jolt.pkgs.cmake
-
-    def decorate(cls):
-        cls = attributes.requires("requires_cmake")(cls)
-        cls.requires_cmake = ["cmake" + (f":version={version}" if version else "")]
-        return cls
-
-    return decorate
