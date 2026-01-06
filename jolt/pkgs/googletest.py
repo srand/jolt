2b45ea4186c32dce1501362b273075a78934b06f
Robert Andersson <rbrt.andersson@outlook.com>
2026-01-07 00:28:50 +0100

    pkgs: googletest: add gmock/main and gtest/main
    Signed-off-by: Robert Andersson <rbrt.andersson@outlook.com>

--- jolt/pkgs/googletest.py
+++ jolt/pkgs/googletest.py
@@ -1,4 +1,4 @@
-from jolt import attributes, Parameter
+from jolt import attributes, Parameter, Task
 from jolt.pkgs import cmake
 from jolt.plugins import git, cmake
 from jolt.tasks import TaskRegistry
@@ -13,5 +13,30 @@ class GoogleTest(cmake.CMake):
     requires_git = ["git:url=https://github.com/google/googletest.git,rev=release-{version}"]
     srcdir = "{git[googletest]}"
 
+    def publish(self, artifact, tools):
+        super().publish(artifact, tools)
+        artifact.cxxinfo.incpaths.append("include")
+        artifact.cxxinfo.libpaths.append("lib")
+        artifact.cxxinfo.libraries.append("gmock")
+        artifact.cxxinfo.libraries.append("gtest")
+
+
+class GTestMain(Task):
+    name = "gtest/main"
+    extends = "googletest"
+
+    def publish(self, artifact, tools):
+        artifact.cxxinfo.libraries.append("gtest_main")
+
+
+class GmockMain(Task):
+    name = "gmock/main"
+    extends = "googletest"
+
+    def publish(self, artifact, tools):
+        artifact.cxxinfo.libraries.append("gmock_main")
+
 
 TaskRegistry.get().add_task_class(GoogleTest)
+TaskRegistry.get().add_task_class(GTestMain)
+TaskRegistry.get().add_task_class(GmockMain)
