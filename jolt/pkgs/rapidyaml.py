ef71017ba650eb69dc924ed5f6c9e1f8f5c21094
Robert Andersson <rbrt.andersson@outlook.com>
2026-01-07 19:17:15 +0100

    pkgs: add rapidyaml
    Signed-off-by: Robert Andersson <rbrt.andersson@outlook.com>

---
+++ jolt/pkgs/rapidyaml.py
@@ -0,0 +1,23 @@
+from jolt import attributes, Parameter
+from jolt.plugins import cmake, git
+from jolt.tasks import TaskRegistry
+
+
+@attributes.requires("requires_src")
+@cmake.requires()
+@cmake.use_ninja()
+class RapidYAML(cmake.CMake):
+    name = "rapidyaml"
+    version = Parameter("0.7.0", help="RapidYAML version.")
+    requires_src = ["git:url=https://github.com/biojppm/rapidyaml.git,rev=v{version},submodules=true"]
+    srcdir = "{git[rapidyaml]}"
+
+    def publish(self, artifact, tools):
+        super().publish(artifact, tools)
+        artifact.cxxinfo.incpaths.append("include")
+        artifact.cxxinfo.libpaths.append("lib")
+        artifact.cxxinfo.libraries.append("ryml")
+        artifact.cxxinfo.libraries.append("c4core")
+
+
+TaskRegistry.get().add_task_class(RapidYAML)
