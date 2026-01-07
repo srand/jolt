8551d4259d5e7acf6572c8f717a953563dfd80b3
Robert Andersson <rbrt.andersson@outlook.com>
2026-01-07 19:11:26 +0100

    pkgs: add mstch
    Signed-off-by: Robert Andersson <rbrt.andersson@outlook.com>

---
+++ jolt/pkgs/mstch.py
@@ -0,0 +1,28 @@
+from jolt import attributes, Parameter
+from jolt.pkgs import boost
+from jolt.plugins import cmake, fetch
+from jolt.tasks import TaskRegistry
+
+
+@attributes.requires("requires_boost")
+@attributes.requires("requires_src")
+@cmake.requires()
+@cmake.use_ninja()
+class MSTCH(cmake.CMake):
+    name = "mstch"
+    version = Parameter("1.0.2", help="MSTCH version.")
+    requires_boost = ["boost"]
+    requires_src = ["fetch:alias=src,url=https://github.com/no1msd/mstch/archive/refs/tags/{version}.tar.gz"]
+    srcdir = "{fetch[src]}/mstch-{version}"
+    options = [
+        "CMAKE_POLICY_VERSION_MINIMUM=3.5",
+    ]
+
+    def publish(self, artifact, tools):
+        super().publish(artifact, tools)
+        artifact.cxxinfo.incpaths.append("include")
+        artifact.cxxinfo.libpaths.append("lib")
+        artifact.cxxinfo.libraries.append("mstch")
+
+
+TaskRegistry.get().add_task_class(MSTCH)
