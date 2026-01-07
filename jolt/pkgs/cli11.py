5a23470f8915dd21f78f892db52ea1c8517407b9
Robert Andersson <rbrt.andersson@outlook.com>
2026-01-07 19:01:53 +0100

    pkgs: add cli11
    Signed-off-by: Robert Andersson <rbrt.andersson@outlook.com>

---
+++ jolt/pkgs/cli11.py
@@ -0,0 +1,23 @@
+from jolt import attributes, Parameter
+from jolt.plugins import cmake, fetch
+from jolt.tasks import TaskRegistry
+
+
+@attributes.requires("requires_src")
+@cmake.requires()
+@cmake.use_ninja()
+class CLI11(cmake.CMake):
+    name = "cli11"
+    version = Parameter("2.6.1", help="CLI11 version.")
+    requires_src = ["fetch:alias=src,url=https://github.com/CLIUtils/CLI11/archive/refs/tags/v{version}.tar.gz"]
+    srcdir = "{fetch[src]}/CLI11-{version}"
+    options = [
+        "CLI11_BUILD_TESTS=OFF",
+    ]
+
+    def publish(self, artifact, tools):
+        super().publish(artifact, tools)
+        artifact.cxxinfo.incpaths.append("include")
+
+
+TaskRegistry.get().add_task_class(CLI11)
