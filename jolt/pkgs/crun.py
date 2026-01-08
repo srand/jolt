5d2e0023244a7bb8d7e92a3750b4da6e49d870a9
Robert Andersson <robert.m.andersson@atlascopco.com>
2026-01-08 12:05:27 +0100

    pkgs: add 'caps' and 'systemd' params to crun
    Signed-off-by: Robert Andersson <robert.m.andersson@atlascopco.com>

--- jolt/pkgs/crun.py
+++ jolt/pkgs/crun.py
@@ -1,21 +1,28 @@
-from jolt import attributes, Parameter
+from jolt import attributes, BooleanParameter, Parameter
+from jolt.pkgs import libcap
 from jolt.plugins import git, autotools, pkgconfig
 from jolt.tasks import TaskRegistry
 
 
 @attributes.requires("requires_git")
+@attributes.requires("requires_libcap_{caps[on,off]}")
 @autotools.requires()
 @pkgconfig.requires()
 class CRun(autotools.Autotools):
     name = "crun"
     version = Parameter("1.26", help="crun version.")
+    caps = BooleanParameter(False, help="Enable capabilities support.")
+    systemd = BooleanParameter(False, help="Enable systemd support.")
     requires_git = ["git:url=https://github.com/containers/crun.git,rev={version},submodules=true"]
+    requires_caps_on = ["libcap"]
     srcdir = "{git[crun]}"
     options = [
         # Unable to get ./configure to pick these up from pkg-config
         "--disable-criu",
         "--disable-seccomp",
         "--enable-embedded-yajl",
+        "{caps[,--disable-caps]}",
+        "{systemd[,--disable-systemd]}",
     ]
 
 
