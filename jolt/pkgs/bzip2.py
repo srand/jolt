a14f4aef28a98bd34f5233b72cf73cbcea48a8f8
Robert Andersson <rbrt.andersson@outlook.com>
2026-01-11 01:35:55 +0100

    pkgs: add libarchive and bzip2
    Signed-off-by: Robert Andersson <rbrt.andersson@outlook.com>

---
+++ jolt/pkgs/bzip2.py
@@ -0,0 +1,43 @@
+from jolt import attributes, BooleanParameter, Parameter
+from jolt.tasks import TaskRegistry
+from jolt.plugins import fetch, ninja
+
+
+@attributes.requires("requires_src")
+@attributes.system
+@ninja.attributes.cflags("cflags_linux_pic_{pic[on,off]}")
+class LibBzip2(ninja.CXXLibrary):
+    name = "libbzip2"
+    binary = "bzip2"
+    version = Parameter("1.0.8", help="bzip2 version.")
+    pic = BooleanParameter(True, help="Build position independent code")
+    shared = BooleanParameter(False, help="Build shared libraries")
+
+    cflags_darwin_pic_on = ["-fPIC"]
+    cflags_linux_pic_on = ["-fPIC"]
+    incremental = False
+    requires_src = ["fetch:alias=src,url=https://sourceware.org/pub/bzip2/bzip2-{version}.tar.gz"]
+    source_influence = False
+    sources = [
+        "{fetch[src]}/bzip2-{version}/blocksort.c",
+        "{fetch[src]}/bzip2-{version}/bzlib.c",
+        "{fetch[src]}/bzip2-{version}/bzlib.h",
+        "{fetch[src]}/bzip2-{version}/compress.c",
+        "{fetch[src]}/bzip2-{version}/crctable.c",
+        "{fetch[src]}/bzip2-{version}/decompress.c",
+        "{fetch[src]}/bzip2-{version}/huffman.c",
+        "{fetch[src]}/bzip2-{version}/randtable.c",
+    ]
+
+    def publish(self, artifact, tools):
+        super().publish(artifact, tools)
+        artifact.cxxinfo.incpaths.append("include")
+        artifact.cxxinfo.libpaths.append("lib")
+        artifact.cxxinfo.libraries.append("bz2")
+        artifact.environ.CMAKE_PREFIX_PATH.append(".")
+
+        with tools.cwd("{fetch[src]}/bzip2-{version}"):
+            artifact.collect("bzlib.h", "include/")
+
+
+TaskRegistry.get().add_task_class(LibBzip2)
---
+++ jolt/pkgs/libarchive.py
@@ -0,0 +1,43 @@
+from jolt import attributes, BooleanParameter, Parameter
+from jolt.tasks import TaskRegistry
+from jolt.plugins import cmake, git
+from jolt.pkgs import bzip2, libxml2, lz4, openssl, xz, zlib, zstd
+
+
+@attributes.requires("requires_bzip2")
+@attributes.requires("requires_libxml2")
+@attributes.requires("requires_lz4")
+@attributes.requires("requires_openssl")
+@attributes.requires("requires_xz")
+@attributes.requires("requires_zlib")
+@attributes.requires("requires_zstd")
+@attributes.requires("requires_git")
+@cmake.requires()
+@cmake.use_ninja()
+class Libarchive(cmake.CMake):
+    name = "libarchive"
+    version = Parameter("3.8.5", help="libarchive version.")
+    shared = BooleanParameter(True, help="Build shared libraries")
+    requires_bzip2 = ["libbzip2"]
+    requires_git = ["git:url=https://github.com/libarchive/libarchive.git,rev=v{version}"]
+    requires_libxml2 = ["libxml2"]
+    requires_lz4 = ["lz4"]
+    requires_openssl = ["openssl"]
+    requires_xz = ["xz"]
+    requires_zlib = ["zlib"]
+    requires_zstd = ["zstd"]
+    srcdir = "{git[libarchive]}"
+    options = [
+        "CMAKE_POLICY_VERSION_MINIMUM=3.5",
+        "BUILD_SHARED_LIBS={shared[ON,OFF]}",
+        "ENABLE_TEST=OFF",
+    ]
+
+    def publish(self, artifact, tools):
+        super().publish(artifact, tools)
+        artifact.cxxinfo.incpaths.append("include")
+        artifact.cxxinfo.libpaths.append("lib")
+        artifact.cxxinfo.libraries.append("archive")
+
+
+TaskRegistry.get().add_task_class(Libarchive)
