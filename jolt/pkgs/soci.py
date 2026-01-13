a1f63c22fc75440095dd1995536dabb9c230c2f3
Robert Andersson <rbrt.andersson@outlook.com>
2026-01-13 22:30:39 +0100

    pkgs: add soci_mysql to soci artifact metadata
    Signed-off-by: Robert Andersson <rbrt.andersson@outlook.com>

--- jolt/pkgs/soci.py
+++ jolt/pkgs/soci.py
@@ -15,8 +15,8 @@ class Soci(cmake.CMake):
     version = Parameter("4.1.2", help="Soci version")
     shared = BooleanParameter(False, help="Build shared libraries")
     boost = BooleanParameter(False, help="Enable boost support")
-    mysql = BooleanParameter(True, "Enable MySQL support")
-    sqlite = BooleanParameter(True, "Enable sqlite support")
+    mysql = BooleanParameter(True, help="Enable MySQL support")
+    sqlite = BooleanParameter(True, help="Enable sqlite support")
 
     requires_boost_on = ["boost"]
     requires_git = ["git:url=https://github.com/SOCI/soci.git,rev=v{version}"]
@@ -28,7 +28,7 @@ class Soci(cmake.CMake):
         "SOCI_SHARED={shared[ON,OFF]}",
         "SOCI_SQLITE3={sqlite[ON,OFF]}",
         "SOCI_MYSQL={mysql[ON,OFF]}",
-        "WITH_BOOST={boost[ON,OFF]}"
+        "WITH_BOOST={boost[ON,OFF]}",
     ]
 
     def publish(self, artifact, tools):
@@ -37,6 +37,8 @@ class Soci(cmake.CMake):
         artifact.cxxinfo.libpaths.append("lib")
         if self.sqlite:
             artifact.cxxinfo.libraries.append("soci_sqlite3")
+        if self.mysql:
+            artifact.cxxinfo.libraries.append("soci_mysql")
         artifact.cxxinfo.libraries.append("soci_odbc")
         artifact.cxxinfo.libraries.append("soci_core")
 
