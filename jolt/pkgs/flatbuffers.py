067362f825e9f88b25af326cbe01d51aef3c5aaa
Robert Andersson <rbrt.andersson@outlook.com>
2026-01-14 00:24:14 +0100

    pkgs: add 'shared' parameter to flatbuffers
    Signed-off-by: Robert Andersson <rbrt.andersson@outlook.com>

--- jolt/pkgs/flatbuffers.py
+++ jolt/pkgs/flatbuffers.py
@@ -1,4 +1,4 @@
-from jolt import attributes, Parameter
+from jolt import attributes, BooleanParameter, Parameter
 from jolt.pkgs import cmake
 from jolt.plugins import git, cmake
 from jolt.tasks import TaskRegistry
@@ -10,9 +10,14 @@ from jolt.tasks import TaskRegistry
 class Flatbuffers(cmake.CMake):
     name = "flatbuffers"
     version = Parameter("25.9.23", help="Flatbuffers version.")
+    shared = BooleanParameter(False, help="Build shared libraries.")
     requires_git = ["git:url=https://github.com/google/flatbuffers.git,rev=v{version}"]
     srcdir = "{git[flatbuffers]}"
-    options = ["FLATBUFFERS_BUILD_TESTS=OFF"]
+    options = [
+        "FLATBUFFERS_BUILD_FLATLIB={shared[OFF,ON]}",
+        "FLATBUFFERS_BUILD_SHAREDLIB={shared[ON,OFF]}",
+        "FLATBUFFERS_BUILD_TESTS=OFF",
+    ]
 
     def publish(self, artifact, tools):
         super().publish(artifact, tools)
