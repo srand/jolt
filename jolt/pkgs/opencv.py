cdc21c59c2ae5c7970f31f7fa1cb616a4f5c87d5
Robert Andersson <rbrt.andersson@outlook.com>
2026-01-13 23:12:36 +0100

    pkgs: add 'shared' parameter to opencv
    Signed-off-by: Robert Andersson <rbrt.andersson@outlook.com>

--- jolt/pkgs/opencv.py
+++ jolt/pkgs/opencv.py
@@ -1,4 +1,4 @@
-from jolt import attributes, Parameter
+from jolt import attributes, BooleanParameter, Parameter
 from jolt.pkgs import cmake, protobuf
 from jolt.plugins import git, cmake
 from jolt.tasks import TaskRegistry
@@ -10,9 +10,15 @@ from jolt.tasks import TaskRegistry
 class OpenCV(cmake.CMake):
     name = "opencv"
     version = Parameter("4.12.0", help="OpenCV version.")
+    shared = BooleanParameter(False, help="Build shared libraries.")
     requires_git = ["git:url=https://github.com/opencv/opencv.git,rev={version}"]
     srcdir = "{git[opencv]}"
-    options = ["BUILD_EXAMPLES=OFF", "BUILD_TESTS=OFF", "BUILD_PERF_TESTS=OFF"]
+    options = [
+        "BUILD_EXAMPLES=OFF", 
+        "BUILD_TESTS=OFF", 
+        "BUILD_PERF_TESTS=OFF",
+        "BUILD_SHARED_LIBS={shared[ON,OFF]}",
+    ]
 
 
 TaskRegistry.get().add_task_class(OpenCV)
