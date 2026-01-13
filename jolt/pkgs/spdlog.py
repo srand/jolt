55e3714608386ec405e9df2110c4632fb0a020e1
Robert Andersson <rbrt.andersson@outlook.com>
2026-01-13 22:27:23 +0100

    pkgs: add 'shared' parameter to spdlog
    Signed-off-by: Robert Andersson <rbrt.andersson@outlook.com>

--- jolt/pkgs/spdlog.py
+++ jolt/pkgs/spdlog.py
@@ -1,4 +1,4 @@
-from jolt import attributes, Parameter
+from jolt import attributes, BooleanParameter, Parameter
 from jolt.pkgs import cmake
 from jolt.plugins import git, cmake
 from jolt.tasks import TaskRegistry
@@ -10,18 +10,20 @@ from jolt.tasks import TaskRegistry
 class Spdlog(cmake.CMake):
     name = "spdlog"
     version = Parameter("1.16.0", help="spdlog version.")
+    shared = BooleanParameter(False, help="Build shared libraries.")
     requires_git = ["git:url=https://github.com/gabime/spdlog.git,rev=v{version}"]
     srcdir = "{git[spdlog]}"
     options = [
-        "SPDLOG_BUILD_TESTS=OFF",
+        "BUILD_SHARED_LIBS={shared[ON,OFF]}",
         "SPDLOG_BUILD_BENCH=OFF",
+        "SPDLOG_BUILD_TESTS=OFF",
     ]
 
     def publish(self, artifact, tools):
         super().publish(artifact, tools)
         artifact.cxxinfo.incpaths.append("include")
         artifact.cxxinfo.libpaths.append("lib")
-        artifact.cxxinfo.libraries.append("stdlog")
+        artifact.cxxinfo.libraries.append("spdlog")
 
 
 TaskRegistry.get().add_task_class(Spdlog)
