3b88349d6a1d6d96e902e4472e194310a6ada23b
Robert Andersson <rbrt.andersson@outlook.com>
2026-01-14 00:38:08 +0100

    pkgs: add 'shared' parameter to ffmpeg
    Signed-off-by: Robert Andersson <rbrt.andersson@outlook.com>

--- jolt/pkgs/ffmpeg.py
+++ jolt/pkgs/ffmpeg.py
@@ -1,4 +1,4 @@
-from jolt import attributes, Parameter
+from jolt import attributes, BooleanParameter, Parameter
 from jolt.pkgs import nasm, pkgconfig, xz, xorg
 from jolt.plugins import git, autotools
 from jolt.tasks import TaskRegistry
@@ -13,11 +13,16 @@ from jolt.tasks import TaskRegistry
 class FFmpeg(autotools.Autotools):
     name = "ffmpeg"
     version = Parameter("8.0.1", help="ffmpeg version.")
+    shared = BooleanParameter(False, help="Build shared libraries.")
     requires_git = ["git:url=https://github.com/FFmpeg/FFmpeg.git,rev=n{version}"]
     requires_linux_libx11 = ["libx11"]
     requires_nasm = ["nasm"]
     requires_xz = ["xz"]
     srcdir = "{git[FFmpeg]}"
+    options = [
+        "--{shared[enable,disable]}-shared",
+        "--{shared[disable,enable]}-static",
+    ]
 
 
 TaskRegistry.get().add_task_class(FFmpeg)
